9735ed3dcdcee500a0f8bf9a2b32ea308b1db1cc
Adrian Velasco <adrivelasco1506@gmail.com>
2019-08-22 10:52:52 -0300

    Change text

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -12,9 +12,19 @@ const App = props => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          <Button styleName="secondary">
-            <Text>asdasda</Text>
-          </Button>
+          <View style={styles.sectionContainer}>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              {'\n'}
+              {'\n'}
+            </Text>
+            <Button styleName="secondary">
+              <Text>Go back</Text>
+            </Button>
+          </View>
         </ScrollView>
       </SafeAreaView>
     </>
